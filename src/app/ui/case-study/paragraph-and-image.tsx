4f248b62937c437c553e254a5f71cb03977bb16f
Joseph Sotelo <josephhsotelo@gmail.com>
2024-02-20 11:24:15 -0800

    developed the paragraph and image component to include a bulleted list

--- src/app/ui/case-study/paragraph-and-image.tsx
+++ src/app/ui/case-study/paragraph-and-image.tsx
@@ -1,14 +1,26 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-export default function ParagraphAndImage({sectionHeader, mainText, image}: {sectionHeader: string, mainText: string, image: string}){
+export default function ParagraphAndImage({sectionHeader, mainText, bullets, image}: {sectionHeader: string, mainText: string, bullets: string[], image: string}){
     return(
         <div className='flex'>
             <div>
-                <h2>
-                    {sectionHeader}
-                </h2>
-                <span>
-                    {mainText}
-                </span>
+                <div>
+                    <h2>
+                        {sectionHeader}
+                    </h2>
+                    <span>
+                        {mainText}
+                        {bullets && (
+                            <ul>
+                                {bullets.map((bullet) => (
+                                    <li>
+                                        {bullet}
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </span>
+                </div>
             </div>
         </div>
     )
--- src/app/work/discover-olvera/page.tsx
+++ src/app/work/discover-olvera/page.tsx
@@ -1,9 +1,44 @@
 'use client'
 
+import Hero from '@/app/ui/case-study/hero';
+import Overview from '@/app/ui/case-study/overview';
+import ParagraphAndImage from '@/app/ui/case-study/paragraph-and-image';
+
+const discoverOlveraData = {
+    hero: {
+        title: 'Discover Olvera',
+        tagline: 'An impactful mobile app that aims to increase foot traffic at Olvera street by gamifying the visitor experience using AR.',
+        occasion: 'IxD Studio 1 // Fall 2023',
+        image: '/../../work/discover-olvera/discover-olvera-cover.png'
+    },
+    overview: {
+        challenge: 'Olvera Street\'s businesses are suffering due to a lingering, pandemic-enduced drop in foot traffic',
+        process: 'Ethnography, Netnography, Prototype, Usability Test',
+        role: 'Design Research, Branding, UX Design',
+    },
+    observations: {
+        sectionHeader: 'Ethnography: Observations',
+        mainText: 'The first stage of our research was ethnography. For us, this meant taking three day-trips to Olvera Street, recording our observations and conducting interviews.',
+        bullets: [
+            'Six shops and one popular resturant were closed down',
+            'There were many transient individuals within Olvera Street, and we witnessed an attempted theft - perhaps they feel emboldened to steal because of the low foot traffic.',
+            'Olvera Street is part of a slightly larger area known as Placita Olvera, which houses five small museums. These Museums are under-promoted, gaining most visitors through outdoor signs, scheduled visits from guided tours, and seasonal events. ',
+        ],
+        image: '',
+    },
+    interviews: {
+        sectionHeader: 'Ethnography: Interviews',
+        mainText: '',
+        image: '',
+    }
+}
+
 export default function Page() {
     return(
-        <h1>
-            Discover Olvera
-        </h1>
+        <div>
+            <Hero {...discoverOlveraData.hero}/>
+            <Overview props={discoverOlveraData.overview}/>
+            <ParagraphAndImage {...discoverOlveraData.observations}/>
+        </div>
     )
 }
