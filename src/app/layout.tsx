7fb337f25d4c853e8ab66fc08e9daf2cafff3194
Joseph Sotelo <josephhsotelo@gmail.com>
2024-04-09 12:14:32 -0700

    feature: add three shadcn components
    Refs:#12

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import MainNav from './ui/main-nav'
-import 'semantic-ui-css/semantic.min.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
