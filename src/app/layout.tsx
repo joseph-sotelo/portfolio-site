af130815e8df46129e016daca41ed0be4f4d14a1
Joseph Sotelo <josephhsotelo@gmail.com>
2024-04-10 13:42:05 -0700

    feature: implement type scale
    #4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Roboto_Mono as FontSans } from 'next/font/google'
 import './globals.css'
 import MainNav from '../components/main-nav'
 
-const inter = Inter({ subsets: ['latin'] })
+import { cn } from "@/lib/utils"
+ 
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +22,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}>
         <div>
           <MainNav />
         </div>
