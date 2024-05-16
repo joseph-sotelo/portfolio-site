6a724c026e0e9a3b03188b41ef0690223eefc931
Joseph Sotelo <josephhsotelo@gmail.com>
2024-05-15 19:34:36 -0700

    hotfix: youtube embed 404
    #21

--- src/components/case-study/case-study-video.tsx
+++ src/components/case-study/case-study-video.tsx
@@ -11,6 +11,7 @@ export default function CaseStudyVideo({header, mainText, src}: {header: string 
                 {mainText}
             </p>
             <iframe className='mt-12 w-full aspect-video rounded-sm' src={src} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+            <iframe width="1184" height="666" src="https://www.youtube.com/embed/ZsmuhoPbTFE" title="Discover Olvera Final Deliverable" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         </div>
     )
 }
