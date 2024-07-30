d792b46c44e3350faaa7b20b022cfa1a774ff9d9
Joseph Sotelo <josephhsotelo@gmail.com>
2024-07-30 15:40:57 -0700

    feature/ create sub-layout.tsx to call client only hooks
    refs: #21

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,7 @@ import type { Metadata } from 'next'
 import { Roboto_Mono as FontSans } from 'next/font/google'
 import { Inter as SecondaryFont } from 'next/font/google'
 import './globals.css'
-
-import ContextMenu from '@/components/context-menu';
-import MobileMenu from '@/components/mobile-menu';
-
-import data from '@/app/content/context-menu.json';
+import SubLayout from './sub-layout'
 
 import { cn } from "@/lib/utils"
  
@@ -32,8 +28,6 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
 
-  const contextMenuData = data.work;
-
   var scrollHeight = 5000;
 
   if (typeof document !== 'undefined') {
@@ -46,19 +40,9 @@ export default function RootLayout({
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}>
-      <div id='layout-grid' className='sm:grid  sm:grid-cols-12 gap-6 mx-6'>
-        <div id='static-menu' className='sm:col-span-4 lg:col-span-3 xl:col-span-2 xl:col-start-2 2xl:col-span-2'>
-          <div className='sm:hidden'>
-            <MobileMenu props={contextMenuData.props}></MobileMenu>
-          </div>
-          <div id='context-menu-wrapper' className='hidden sm:block h-full'>
-              <ContextMenu props={contextMenuData.props} isInvisible={true} hideTitleAtStart={false}></ContextMenu>
-          </div>
-        </div>
-        <div id='content' className='mr-auto ml-auto max-w-[373px] sm:ml-0 sm:max-w-full sm:col-span-8 2xl:col-span-10'> 
-            {children}
-        </div>
-      </div>
+          <SubLayout children={children}>
+
+          </SubLayout>
       </body>
     </html>
   )
---
+++ src/app/sub-layout.tsx
@@ -0,0 +1,30 @@
+import ContextMenu from '@/components/context-menu';
+import MobileMenu from '@/components/mobile-menu';
+
+import data from '@/app/content/context-menu.json';
+
+export default function SubLayout ({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+
+    const contextMenuData = data.work;
+
+  return (
+      <div id='layout-grid' className='sm:grid  sm:grid-cols-12 gap-6 mx-6'>
+      <div id='static-menu' className='sm:col-span-4 lg:col-span-3 xl:col-span-2 xl:col-start-2 2xl:col-span-2'>
+        <div className='sm:hidden'> 
+          <MobileMenu props={contextMenuData.props}></MobileMenu>
+        </div>
+        <div id='context-menu-wrapper' className='hidden sm:block h-full'>
+            <ContextMenu props={contextMenuData.props} isInvisible={true} hideTitleAtStart={false}></ContextMenu>
+        </div>
+      </div>
+      <div id='content' className='mr-auto ml-auto max-w-[373px] sm:ml-0 sm:max-w-full sm:col-span-8 2xl:col-span-10'> 
+          {children}
+      </div>
+    </div>
+  )
+
+}
