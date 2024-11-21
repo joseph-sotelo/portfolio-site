03c20e9073bdc97664987aa3c3720f8af07a3d3a
Joseph Sotelo <josephhsotelo@gmail.com>
2024-11-20 20:59:31 -0800

    -m”hotfix: video type mismatches
    refs: #25

--- src/components/component-types.tsx
+++ src/components/component-types.tsx
@@ -15,6 +15,9 @@ export type ScrollSectionType = {
     sources?: string[];
     accordionSections?: string[][];
     lines?: string[][];
+    isAutoPlay?: boolean;
+    showControls?: boolean;
+    isMuted?: boolean;
 }
 
 export type CaseStudyCardType = {
@@ -37,6 +40,6 @@ export type CaseStudyType = {
     sections: ScrollSectionType[];
 }
 
-export type CaseStudyPageKeyType = 'discover-olvera' | 'voxai' | 'slimbox' | 'other';
+export type CaseStudyPageKeyType = 'discover-olvera' | 'voxai' | 'slimbox' | 'other' | 'semiotic-clock' | 'electric-spoons';
 
-export type GlobalPageKeyType = 'discover-olvera' | 'voxai' | 'slimbox' | 'other' | '' | 'about';
+export type GlobalPageKeyType = 'discover-olvera' | 'voxai' | 'slimbox' | 'other' | '' | 'about' | 'semiotic-clock' | 'electric-spoons';
