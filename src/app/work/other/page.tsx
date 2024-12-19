e40d344cf62a57ea31e9b7c7f0e3ac6c23002817
Joseph Sotelo <josephhsotelo@gmail.com>
2024-12-18 16:55:09 -0800

    create bird feeder case study

--- src/app/work/other/page.tsx
+++ src/app/work/other/page.tsx
@@ -1,4 +1,29 @@
+"use client"
+
 import { MainLayout } from "@/components/main-layout"
+ 
+import { Bar, BarChart } from "recharts"
+import { ChartConfig, ChartContainer } from "@/components/ui/chart"
+
+const chartConfig = {
+    desktop: {
+      label: "Desktop",
+      color: "#2563eb",
+    },
+    mobile: {
+      label: "Mobile",
+      color: "#60a5fa",
+    },
+  } satisfies ChartConfig
+
+const chartData = [
+    { month: "January", desktop: 186, mobile: 80 },
+    { month: "February", desktop: 305, mobile: 200 },
+    { month: "March", desktop: 237, mobile: 120 },
+    { month: "April", desktop: 73, mobile: 190 },
+    { month: "May", desktop: 209, mobile: 130 },
+    { month: "June", desktop: 214, mobile: 140 },
+  ]
 
 export default function Page(){
 
@@ -7,6 +32,14 @@ export default function Page(){
             <h1 className='opacity-20 mt-[21vh] text-center w-full'>
                 gallery incoming
             </h1>
+
+            <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+                <BarChart accessibilityLayer data={chartData}>
+                    <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+                    <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+                </BarChart>
+            </ChartContainer>
+
         </MainLayout>
     )
 }
