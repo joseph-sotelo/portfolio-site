7ee910bac9c7aa449260ed0bd39af07f9e4d1697
joseph-sotelo <josephhsotelo@gmail.com>
2025-09-22 12:19:35 -0700

    set up case study

---
+++ src/app/work/template-designer/page.tsx
@@ -0,0 +1,11 @@
+import ScrollSectionsFormatter from "@/components/scroll-sections/scroll-sections-formatter"
+import { MainLayout } from "@/components/main-layout"
+
+export default function Page(){
+
+    return(
+        <MainLayout>
+            <ScrollSectionsFormatter />
+        </MainLayout>
+    )
+}
